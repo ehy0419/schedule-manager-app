37ce9c70778abc77c49b714f73e04d9eac7c895d
ehy0419 <2hy0419@naver.com>
2025-08-01 18:21:07 +0900

    feat. 엔티티

---
+++ src/main/java/com/schedulemanagerapp/entity/Schedule.java
@@ -0,0 +1,25 @@
+package com.schedulemanagerapp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String scheduleName;
+    private String scheduleDescription;
+    private LocalDateTime scheduleStartTime;
+    private LocalDateTime scheduleEndTime;
+}
