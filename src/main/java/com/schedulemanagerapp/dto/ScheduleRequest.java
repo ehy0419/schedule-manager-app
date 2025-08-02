82332a0f289c8f3173f5e9ac78384efbf87eae4f
ehy0419 <2hy0419@naver.com>
2025-08-02 20:22:09 +0900

    feat. 요청 dto

--- src/main/java/com/schedulemanagerapp/dto/ScheduleRequest.java
+++ src/main/java/com/schedulemanagerapp/dto/ScheduleRequest.java
@@ -1,14 +1,15 @@
 package com.schedulemanagerapp.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
+@NoArgsConstructor
 public class ScheduleRequest {
 
     private String scheduleName;
     private String scheduleDescription;
-    private LocalDateTime scheduleStartTime;
-    private LocalDateTime scheduleEndTime;
+    private LocalDateTime scheduleTime;
 }
