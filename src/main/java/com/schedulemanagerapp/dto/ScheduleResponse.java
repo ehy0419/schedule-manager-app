465bc7e833147770b6308b997405015f41d31ec4
ehy0419 <2hy0419@naver.com>
2025-08-02 20:23:03 +0900

    feat. 반응 dto

--- src/main/java/com/schedulemanagerapp/dto/ScheduleResponse.java
+++ src/main/java/com/schedulemanagerapp/dto/ScheduleResponse.java
@@ -2,14 +2,22 @@ package com.schedulemanagerapp.dto;
 
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class ScheduleResponse {
 
-    private final Long scheduleId;
-    private final String scheduleName;
+    private final Long id;
+    private final String title;
+    private final String description;
+    private final LocalDateTime scheduleTime;
 
-    public ScheduleResponse(Long scheduleId, String scheduleName) {
-        this.scheduleId = scheduleId;
-        this.scheduleName = scheduleName;
+
+    public ScheduleResponse(Long id, String title, String description, LocalDateTime scheduleTime) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.scheduleTime = scheduleTime;
     }
+
 }
