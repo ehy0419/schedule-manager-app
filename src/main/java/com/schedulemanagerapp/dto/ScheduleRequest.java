de3cfc98e2a329e039261eea701b99f9f1988c49
ehy0419 <2hy0419@naver.com>
2025-08-04 11:18:10 +0900

    feat. 레벨 7 글자 수 제한

--- src/main/java/com/schedulemanagerapp/dto/ScheduleRequest.java
+++ src/main/java/com/schedulemanagerapp/dto/ScheduleRequest.java
@@ -17,18 +17,18 @@ public class ScheduleRequest {
 
         // 조건 1 : 일정 제목은 최대 30자 이내, 필수값
         if (title == null || title.isEmpty()) {
-            return "Title required";
+            return "일정 제목은 필수입니다.";
         }
         if (title.length() > 30) {
-            return "Title length exceeds";
+            return "일정 제목은 최대 30글자까지 입력 가능합니다.";
         }
 
         // 조건 2 : 일정 내용은 최대 200자 이내, 필수값
         if (description == null || description.isEmpty()) {
-            return "Description required";
+            return "일정 내용은 필수입니다.";
         }
         if (description.length() > 200) {
-            return "Description length exceeds";
+            return "일정 내용은 최대 200글자까지 입력 가능합니다.";
         }
 
         // 조건 3 : 일정 시간은 필수 - '현재 시간 이후로만 일정을 등록할 수 있어요.'
